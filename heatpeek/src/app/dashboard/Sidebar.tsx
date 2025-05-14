7c13d894218f1ad10738c540919d22cc7093ddfe
Guillaume Meheut <guillaume.meheutpro@gmail.com>
2025-05-14 06:00:29 +0200

    fix: correct case sensitivity for component files

---
+++ heatpeek/src/app/dashboard/Sidebar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { UrlList } from "@/components/UrlList";
+import { SnapshotUrl } from "@/lib/supabase/queries";
+import { Separator } from "@/components/ui/separator";
+
+export function Sidebar({ snapshotsUrls }: { snapshotsUrls: SnapshotUrl[] }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <div
+      className={`relative top-0 left-0 h-screen p-4 transition-all duration-300 ease-in-out ${
+        isSidebarOpen ? "w-64" : "w-12"
+      } border-r bg-primary/5 `}
+    >
+      <Button
+        variant="ghost"
+        size="icon"
+        className="absolute -right-4 top-1/2 -translate-y-1/2 z-10 h-8 w-8 rounded-full border bg-background"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      >
+        {isSidebarOpen ? (
+          <ChevronLeft className="h-4 w-4" />
+        ) : (
+          <ChevronRight className="h-4 w-4" />
+        )}
+      </Button>
+
+      {isSidebarOpen && (
+        <div className="relative space-y-4">
+          <Button asChild>
+            <Link href="/add-page" className="flex items-center gap-2 w-full">
+              <Plus className="h-4 w-4" />
+              New Page
+            </Link>
+          </Button>
+        </div>
+      )}
+
+      <Separator orientation="horizontal" className="w-full my-4" />
+
+      <UrlList isVisible={isSidebarOpen} snapshotsUrls={snapshotsUrls} />
+    </div>
+  );
+}
